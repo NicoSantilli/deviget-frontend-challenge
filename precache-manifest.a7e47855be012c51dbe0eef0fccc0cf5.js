b469625f029ae4c6098cb4726819ff1b8bc20566
NicoSantilli <nasmaturana@gmail.com>
2020-06-04 23:50:35 -0300

    Updates

---
+++ precache-manifest.a7e47855be012c51dbe0eef0fccc0cf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528d4fca126e14f87fc816353c4895c7",
+    "url": "/deviget-frontend-challenge/index.html"
+  },
+  {
+    "revision": "4a578db424c42d3c4593",
+    "url": "/deviget-frontend-challenge/static/js/2.d41e93b4.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/deviget-frontend-challenge/static/js/2.d41e93b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c0e3bda91cefe9cae7",
+    "url": "/deviget-frontend-challenge/static/js/main.47575857.chunk.js"
+  },
+  {
+    "revision": "bbb771223949afe3dae4",
+    "url": "/deviget-frontend-challenge/static/js/runtime-main.6ea1a0e7.js"
+  }
+]);
